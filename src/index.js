2d34df169bef8c8f2f629a10da1fc5171c10aa38
Eralp Sahin <eralpsahin@me.com>
2019-07-04 23:34:33 -0400

    Initial skeleton commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * This function says hello.
+ * @param {string} name Some name to say hello for.
+ * @returns {string} The hello.
+ */
+const sayHello = (name = 'Haz') => `Hello, ${name}!`;
+
+export default sayHello;
