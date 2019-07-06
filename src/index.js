74d4afb5d22b95c5a5e7d17c6fa58ba5d34b9b0c
Eralp Sahin <eralpsahin@me.com>
2019-07-05 23:24:55 -0400

    Add MySU authentication

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-/**
- * This function says hello.
- * @param {string} name Some name to say hello for.
- * @returns {string} The hello.
- */
-const sayHello = (name = 'Haz') => `Hello, ${name}!`;
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-export default sayHello;
+const instance = axios.create({
+  baseURL: 'https://mysu.sabanciuniv.edu/wsdl/mysuapp/v3/',
+  headers: { 'Content-Type': 'text/xml' }
+});
+
+const mysu = {};
+
+/**
+ * This function authenticates the user to
+ * MySU app.
+ * @param {string} username SU-Net username
+ * @param {string} password SU-Net password
+ * @returns {Number} Error code of the response
+ */
+mysu.authenticate = async (username, password) => {
+  const body = `<?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+          <checkLogin xmlns="http://tempuri.org/">
+              <code>@+PQaUrN8Y[dsF79W</code>
+              <uid>${username}</uid>
+              <pwd>${password}</pwd>
+          </checkLogin>
+      </soap:Body>
+    </soap:Envelope>`;
+
+  const response = await instance.post('/authentication.php', body, {
+    transformResponse: data => {
+      const $ = cheerio.load(data);
+      return +$('errorcode').html();
+    }
+  });
+  return response.data;
+};
+
+export default mysu;
