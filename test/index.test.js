6c3a198936efca9146a40081565e69b89a4f2574
Eralp Sahin <eralpsahin@me.com>
2019-07-06 21:35:39 -0400

    Add successful auth test

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,22 @@
-import mysu from '../src';
+import { resolve } from 'path';
+import { config } from 'dotenv';
+import mysu from '../src/index';
 
-test('Failed Authentication', async () => {
+config({ path: resolve(__dirname, '../.env') });
+
+test('empty credentials authentication', async () => {
   expect(await mysu.authenticate()).toBe(1);
+});
+
+test('wrong credentials authentication', async () => {
   expect(await mysu.authenticate('test', 'test')).toBe(1);
+});
+
+const conditional =
+  !!process.env.AUTH_USER && !!process.env.AUTH_PASS ? test : test.skip;
+
+conditional('correct credentials authentication', async () => {
+  expect(
+    await mysu.authenticate(process.env.AUTH_USER, process.env.AUTH_PASS)
+  ).toBe(0);
 });
